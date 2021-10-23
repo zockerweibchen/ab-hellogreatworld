ab0df446fb7ef1d06f03adcc939620150d152c77
ablr59 <angelina.bieler@mnd.thm.de>
2021-10-23 13:36:22 +0200

    HelloGreatWorld

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("It took me a long time to get online and since than I'm getting more and more eager to know who will call.");
         System.out.println("What do you say? Am I doing a good Job?");
         System.out.println("Have a nice Day.");
+
     }
 }
