fc72b3a04c843a363eebdf9cdb379a816a295f34
ablr59 <angelina.bieler@mnd.thm.de>
2021-10-23 12:47:13 +0200

    HelloGreatWorld

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Torsten,");
+        System.out.println("I am so happy that you called me.");
+        System.out.println("It took me a long time to get online and since than I'm getting more and more eager to know who will call.");
+        System.out.println("What do you say? Am I doing a good Job?");
+        System.out.println("Have a nice Day.");
+    }
+}
